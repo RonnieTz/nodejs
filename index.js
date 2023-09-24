4813159fda22cd0293e6f1564bc0f58be8a302b7
RonnieTz <ronis.tzol@gmail.com>
2023-09-24 15:08:18 +0100

    2nd commit

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json("server on");
 });
+app.get("/home", (req, res) => {
+  res.json("server on home");
+});
 
 app.listen(3000, () => {
   console.log("Server online");
