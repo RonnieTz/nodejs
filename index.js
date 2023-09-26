9aba1e87ac742430a800b53f148d653ce42d7825
RonnieTz <ronis.tzol@gmail.com>
2023-09-26 20:04:23 +0100

    chenge link

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const cors = require("cors");
+app.use(cors({ origin: ["https://thequizgame.vercel.app"] }));
 
 io.on("connection", (socket) => {
   socket.emit("connect", { message: `Client ${socket.id} connected.` });
