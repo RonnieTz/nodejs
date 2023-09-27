5d9eee75704b15126716bbedffc5c1db9f12f372
RonnieTz <ronis.tzol@gmail.com>
2023-09-27 17:08:16 +0100

    post request

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const app = require("express")();
 const http = require("http").createServer(app, { cors: { origin: "*" } });
 const io = require("socket.io")(http, { cors: { origin: "*" } });
+const cors = require("cors");
+
+app.use(cors({ origin: "*" }));
 
 io.on("connection", (socket) => {
   console.log(socket.id);
+});
+
+app.post("/", (req, res) => {
+  res.json("server response");
 });
 
 http.listen(3000, () => {
