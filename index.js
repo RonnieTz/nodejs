58f16f53191c569cb140439a3525e31efd5b07b5
RonnieTz <ronis.tzol@gmail.com>
2023-09-27 17:20:42 +0100

    body response

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const app = require("express")();
 const http = require("http").createServer(app, { cors: { origin: "*" } });
 const io = require("socket.io")(http, { cors: { origin: "*" } });
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
 
 io.on("connection", (socket) => {
   console.log(socket.id);
 });
 
 app.post("/", (req, res) => {
-  res.json("server response");
+  res.json(req.body);
 });
 
 http.listen(3000, () => {
