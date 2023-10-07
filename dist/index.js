8dcd6718759d859aef96253fa1a12d53ac53ae8b
RonnieTz <ronis.tzol@gmail.com>
2023-10-07 15:59:35 +0100

    express type

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,32 @@
-// const app = require("express")();
+import { createServer } from "http";
+const app = require("express")();
 // const http = require("http").createServer(app, { cors: { origin: "*" } });
-import express, { Server } from "socket.io";
+import { Server } from "socket.io";
 import cors from "cors";
 import bodyParser from "body-parser";
+// const app: express.Application = express();
+const http = createServer(app);
 const io = new Server(http, { cors: { origin: "*" } });
 app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
 app.use(bodyParser.json());
 app.set("io", io);
 io.on("connection", (socket) => {
-  console.log(socket.id);
+    console.log(socket.id);
 });
 app.post("/", (req, res) => {
-  io.emit("data", req.body);
-  res.json(req.body);
+    io.emit("data", req.body);
+    res.json(req.body);
 });
 app.post("/rooms", (req, res) => {
-  console.log(req.body);
-  io.emit("rooms", req.body);
-  res.json(req.body);
+    console.log(req.body);
+    io.emit("rooms", req.body);
+    res.json(req.body);
 });
 app.post("/adduser", (req, res) => {
-  io.emit("adduser");
-  res.json(req.body);
+    io.emit("adduser");
+    res.json(req.body);
 });
 http.listen(3001, () => {
-  console.log("server online");
+    console.log("server online");
 });
 //# sourceMappingURL=index.js.map
