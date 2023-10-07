334646c282c9ab2191e5873c6d63de365021d1da
RonnieTz <ronis.tzol@gmail.com>
2023-10-07 15:41:09 +0100

    more changes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,29 @@
-import express from "express";
-const app = express();
-app.listen(3000, () => {
-    console.log("Server online");
+const app = require("express")();
+const http = require("http").createServer(app, { cors: { origin: "*" } });
+import { Server } from "socket.io";
+import cors from "cors";
+import bodyParser from "body-parser";
+const io = new Server(http, { cors: { origin: "*" } });
+app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
+app.use(bodyParser.json());
+app.set("io", io);
+io.on("connection", (socket) => {
+    console.log(socket.id);
+});
+app.post("/", (req, res) => {
+    io.emit("data", req.body);
+    res.json(req.body);
+});
+app.post("/rooms", (req, res) => {
+    console.log(req.body);
+    io.emit("rooms", req.body);
+    res.json(req.body);
+});
+app.post("/adduser", (req, res) => {
+    io.emit("adduser");
+    res.json(req.body);
+});
+http.listen(3001, () => {
+    console.log("server online");
 });
 //# sourceMappingURL=index.js.map
