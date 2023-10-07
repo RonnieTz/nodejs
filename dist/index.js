544a33f2c3eeb7569492ed2327a3256dd0136423
RonnieTz <ronis.tzol@gmail.com>
2023-10-07 16:14:40 +0100

    fixed typesxript

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,17 @@
+import express from "express";
 import { createServer } from "http";
-const app = require("express")();
-// const http = require("http").createServer(app, { cors: { origin: "*" } });
-import { Server } from "socket.io";
 import cors from "cors";
+import { Server } from "socket.io";
 import bodyParser from "body-parser";
-// const app: express.Application = express();
+const app = express();
 const http = createServer(app);
 const io = new Server(http, { cors: { origin: "*" } });
-app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
-app.set("io", io);
 io.on("connection", (socket) => {
     console.log(socket.id);
 });
-app.post("/", (req, res) => {
-    io.emit("data", req.body);
-    res.json(req.body);
-});
-app.post("/rooms", (req, res) => {
-    console.log(req.body);
-    io.emit("rooms", req.body);
-    res.json(req.body);
-});
-app.post("/adduser", (req, res) => {
-    io.emit("adduser");
-    res.json(req.body);
-});
-http.listen(3001, () => {
-    console.log("server online");
+http.listen(3000, () => {
+    console.log("Server Online");
 });
 //# sourceMappingURL=index.js.map
