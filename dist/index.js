676fbb884278e97a9bcefe870d9a3bd0c2b457a0
RonnieTz <ronis.tzol@gmail.com>
2023-10-09 17:37:16 +0100

    fixed bug

--- dist/index.js
+++ dist/index.js
@@ -54,7 +54,7 @@ app.post("/exitroom", (req, res) => {
     io.emit("rooms", rooms);
     res.json(rooms);
 });
-app.post("deleteroom", (req, res) => {
+app.post("/deleteroom", (req, res) => {
     const { room } = req.body;
     const index = rooms.findIndex((item) => item.id === room);
     rooms.splice(index, 1);
--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ app.post("/exitroom", (req, res) => {
   res.json(rooms);
 });
 
-app.post("deleteroom", (req, res) => {
+app.post("/deleteroom", (req, res) => {
   const { room }: { room: string } = req.body;
   const index = rooms.findIndex((item) => item.id === room);
   rooms.splice(index, 1);
